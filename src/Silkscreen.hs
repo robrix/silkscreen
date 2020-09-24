17a5bdf3057d93c90cdb50d61b5edd8c2fc02254
Rob Rix <rob.rix@me.com>
2020-09-24 13:41:04 -0400

    :memo: liftDoc1.

--- src/Silkscreen.hs
+++ src/Silkscreen.hs
@@ -53,6 +53,7 @@ class Monoid p => Printer p where
   -- | Lift a 'P.Doc' to a 'Printer'.
   liftDoc0 :: P.Doc (Ann p) -> p
 
+  -- | Lift a unary function on 'P.Doc' to a 'Printer'.
   liftDoc1 :: (P.Doc (Ann p) -> P.Doc (Ann p)) -> (p -> p)
 
   liftDoc2 :: (P.Doc (Ann p) -> P.Doc (Ann p) -> P.Doc (Ann p)) -> (p -> p -> p)
