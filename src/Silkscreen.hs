4dd02abba77c5167c5929eaffc73aaaa36ffa08a
Rob Rix <rob.rix@me.com>
2020-09-24 09:14:46 -0400

    Define a Printer instance for Doc.

--- src/Silkscreen.hs
+++ src/Silkscreen.hs
@@ -3,5 +3,10 @@ module Silkscreen
 ( Printer(..)
 ) where
 
+import qualified Prettyprinter as P
+
 class Printer p where
   type Ann p
+
+instance Printer (P.Doc ann) where
+  type Ann (P.Doc ann) = ann
