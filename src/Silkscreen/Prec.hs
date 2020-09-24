d2ec6bf3449555e155bc8fb04b029e4d352c9582
Rob Rix <rob.rix@me.com>
2020-09-24 10:44:46 -0400

    :memo: Level.

--- src/Silkscreen/Prec.hs
+++ src/Silkscreen/Prec.hs
@@ -6,6 +6,9 @@ module Silkscreen.Prec
 import Silkscreen
 
 class Printer p => PrecPrinter p where
+  -- | The type used to represent precedence levels. This is defined as an associated type so that consumers can use e.g. symbolic representations of their DSL’s precedence levels instead of e.g. unsemantic 'Int's.
+  --
+  -- This type will usually be 'Ord'ered, but this isn’t strictly required so that other means of determining precedence can be provided.
   type Level p
 
   -- | Print informed by the current 'Level'.
