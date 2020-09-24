7816561273b161c99d6c73a194cfb2ccead07b0e
Rob Rix <rob.rix@me.com>
2020-09-24 14:09:41 -0400

    Define an eliminator for Fresh.

--- src/Silkscreen/Printer/Fresh.hs
+++ src/Silkscreen/Printer/Fresh.hs
@@ -1,6 +1,10 @@
 module Silkscreen.Printer.Fresh
 ( -- * Printer binding fresh variables
-  Fresh(..)
+  runFresh
+, Fresh(..)
 ) where
+
+runFresh :: Int -> Fresh p -> p
+runFresh v (Fresh run) = run v
 
 newtype Fresh p = Fresh (Int -> p)
