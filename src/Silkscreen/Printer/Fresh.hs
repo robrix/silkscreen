24ea0775a9af6341c2db3cdeea82e9211e72e0af
Rob Rix <rob.rix@me.com>
2020-09-24 14:12:30 -0400

    Define a Printer instance for Fresh.

--- src/Silkscreen/Printer/Fresh.hs
+++ src/Silkscreen/Printer/Fresh.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
 module Silkscreen.Printer.Fresh
 ( -- * Printer binding fresh variables
   runFresh
@@ -7,6 +8,7 @@ module Silkscreen.Printer.Fresh
 , module Silkscreen.Fresh
 ) where
 
+import Control.Applicative (liftA2)
 import Silkscreen.Fresh
 
 runFresh :: Int -> Fresh p -> p
@@ -14,3 +16,14 @@ runFresh v (Fresh run) = run v
 
 newtype Fresh p = Fresh (Int -> p)
   deriving (Applicative, Functor, Monad, Monoid, Semigroup)
+
+instance Printer p => Printer (Fresh p) where
+  type Ann (Fresh p) = Ann p
+
+  liftDoc0 = pure . liftDoc0
+  liftDoc1 = fmap . liftDoc1
+  liftDoc2 = liftA2 . liftDoc2
+
+  parens = fmap parens
+  brackets = fmap brackets
+  braces = fmap braces
