94cff124ec0005d2670c0bc293a1a3b3198db047
Rob Rix <rob.rix@me.com>
2020-09-24 11:57:46 -0400

    Define a Rainbow printer.

--- src/Silkscreen/Rainbow.hs
+++ src/Silkscreen/Rainbow.hs
@@ -1,6 +1,8 @@
 module Silkscreen.Rainbow
 ( -- * Printing with nesting levels
   RainbowPrinter(..)
+  -- * Rainbow parentheses
+, Rainbow(..)
 ) where
 
 import Silkscreen
@@ -10,3 +12,6 @@ class Printer p => RainbowPrinter p where
   --
   -- This should be used inside parentheses, brackets, braces, etc., and will inform the annotation of their delimiters.
   incrNesting :: p -> p
+
+
+newtype Rainbow a = Rainbow (Int -> a)
