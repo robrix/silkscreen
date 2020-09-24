fdc4a2e0c722bce27e5b8ec2cc4bfea61190e480
Rob Rix <rob.rix@me.com>
2020-09-24 10:52:59 -0400

    :memo: PrecPrinter.

--- src/Silkscreen/Prec.hs
+++ src/Silkscreen/Prec.hs
@@ -5,6 +5,29 @@ module Silkscreen.Prec
 
 import Silkscreen
 
+-- | Pretty-printing with parenthesis insertion resolving precedence.
+--
+-- Given:
+--
+-- @
+-- data ArithLevel = Add | Mult | Exp
+--   deriving (Eq, Ord)
+--
+-- (+.) :: (PrecPrinter p, Level p ~ ArithLevel) => p -> p -> p
+-- a +. b = 'prec' Add $ a '<+>' 'pretty' "+" '<+>' b
+-- infixl 6 +.
+--
+-- (*.) :: (PrecPrinter p, Level p ~ ArithLevel) => p -> p -> p
+-- a *. b = 'prec' Mult $ a '<+>' 'pretty' "*" '<+>' b
+-- infixl 7 *.
+--
+-- (^.) :: (PrecPrinter p, Level p ~ ArithLevel) => p -> p -> p
+-- a ^. b = 'prec' Exp $ a '<+>' 'pretty' "^" '<+>' b
+-- infixl 8 ^.
+-- @
+--
+-- >>> (pretty "a" +. pretty "b") *. pretty "c" ^. (pretty "d" *. pretty "e")
+-- (a + b) * c ^ (d * e)
 class Printer p => PrecPrinter p where
   -- | The type used to represent precedence levels. This is defined as an associated type so that consumers can use e.g. symbolic representations of their DSL’s precedence levels instead of e.g. unsemantic 'Int's.
   --
