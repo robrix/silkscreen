52e7d188f3ee6de501c1337841c7ba7e5aefbc22
Rob Rix <rob.rix@me.com>
2020-09-24 14:10:01 -0400

    Re-export Silkscreen.Fresh.

--- src/Silkscreen/Printer/Fresh.hs
+++ src/Silkscreen/Printer/Fresh.hs
@@ -2,7 +2,11 @@ module Silkscreen.Printer.Fresh
 ( -- * Printer binding fresh variables
   runFresh
 , Fresh(..)
+  -- * Re-exports
+, module Silkscreen.Fresh
 ) where
+
+import Silkscreen.Fresh
 
 runFresh :: Int -> Fresh p -> p
 runFresh v (Fresh run) = run v
