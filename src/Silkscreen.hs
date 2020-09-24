e9b8c34e5ccfb1a760b2d14e53e8278e57e51b46
Rob Rix <rob.rix@me.com>
2020-09-24 09:15:05 -0400

    Define a parens combinator.

--- src/Silkscreen.hs
+++ src/Silkscreen.hs
@@ -8,5 +8,10 @@ import qualified Prettyprinter as P
 class Printer p where
   type Ann p
 
+  parens :: p -> p
+
+
 instance Printer (P.Doc ann) where
   type Ann (P.Doc ann) = ann
+
+  parens = P.parens
