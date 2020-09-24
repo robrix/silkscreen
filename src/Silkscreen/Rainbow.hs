466fb7e927cf637039e2d44084a92cabee720a3f
Rob Rix <rob.rix@me.com>
2020-09-24 11:55:32 -0400

    :memo: incrNesting.

--- src/Silkscreen/Rainbow.hs
+++ src/Silkscreen/Rainbow.hs
@@ -5,4 +5,7 @@ module Silkscreen.Rainbow
 import Silkscreen
 
 class Printer p => RainbowPrinter p where
+  -- | Increment the nesting level of a printer.
+  --
+  -- This should be used inside parentheses, brackets, braces, etc., and will inform the annotation of their delimiters.
   incrNesting :: p -> p
