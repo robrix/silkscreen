d8bfa7d73c41af3eb7a23de506a92c6239ca0553
Rob Rix <rob.rix@me.com>
2020-09-24 11:39:01 -0400

    Define a Monad instance for Prec.

--- src/Silkscreen/Prec.hs
+++ src/Silkscreen/Prec.hs
@@ -98,4 +98,4 @@ runPrec :: level -> Prec level a -> a
 runPrec level (Prec run) = run level
 
 newtype Prec level a = Prec (level -> a)
-  deriving (Applicative, Functor, Monoid, Semigroup)
+  deriving (Applicative, Functor, Monad, Monoid, Semigroup)
